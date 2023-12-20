fa51733a956095b94d70b680aa74033fbcf900e5
Steven Mentzer <stevenfmentzer@gmail.com>
2023-12-20 11:03:04 -0500

    incomplete deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [sushisArray, setSushisArray] = useState([])
+  const [sushiPrice, setSushiPrice] = useState(0)
+  const [plates, setPlates] =[]
+
+
+  useEffect(() => {
+   fetch("http://localhost:3001/sushis")
+   .then(res => res.json())
+   .then(setSushisArray)
+  },[])
+
+
+  function handleSushiClick(event){
+    console.log(event.target)
+  }
+  function sushiEaten(){ 
+   // on click the value of sushi price is returned and subtracted by 
+    //a total (state in App.js) that is passed as prop to Table.js)
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer API={API} sushisArray={sushisArray}/>
+      <Table plates={plates} sushiPrice={sushiPrice}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer( {API, sushisArray}) {
+
+const [arrayTracker, setArrayTracker] = useState(4)
+const [displayArray, setDisplayArray] = useState([])
+
+ function handleMoreButton(){
+  setArrayTracker(arrayTracker+4)
+  handleSetDisplayArray()
+ }
+
+ function handleSetDisplayArray(){
+  let newArray = sushisArray.slice(arrayTracker-4,arrayTracker)
+  newArray = newArray.map((sushi) => {
+    return(
+    <Sushi 
+    key={sushi.id} 
+    sushi={sushi} 
+    handleSushiClick={handleSushiClick}/>
+    );
+  })
+  setDisplayArray(newArray)
+ }
+
+
+ function handleSushiClick(id){
+  console.log(id)
+  
+ }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {displayArray}
+      <MoreButton handleMoreButton={handleMoreButton}/>
     </div>
   );
-}
+  }
 
 export default SushiContainer;
